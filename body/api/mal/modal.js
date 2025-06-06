3b2fefba4b042bf1d867a97a27851c423730cf69
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2025-06-06 16:16:33 +0300

    Update modal.js

--- body/api/mal/modal.js
+++ body/api/mal/modal.js
@@ -1,4 +1,4 @@
-import {El} from '../../../base/classes/mjs.js';
+import {El} from '../../../base/classes/m.js';
 import {Modal} from '../def/modal.js';
 import {MalFc as MalApi} from '../../../api/mal/fc.js';
 
