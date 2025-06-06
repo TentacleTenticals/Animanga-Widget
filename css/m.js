2d664e9d3425e8211e32a7cf258353b3ad769227
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2025-06-06 16:45:14 +0300

    Update m.js

--- css/m.js
+++ css/m.js
@@ -1,4 +1,4 @@
-export const css = () => `
+export default () => `
 @import url('https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Parkinsans:wght@300..800&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Faculty+Glyphic&display=swap');
