bfd36f248d537c399d12be52fe39f67ae2893fad
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2025-06-08 14:38:08 +0300

    Update m.js

--- body/api/def/m.js
+++ body/api/def/m.js
@@ -322,7 +322,7 @@ export class Def{
         func: (e) => {
           el.def.reload = e;
           el.def.runner = () => this.run(o);
-          el.def.closest('.animanga-widget').addEventListener('rel', () => {
+          p.closest('.animanga-widget').addEventListener('rel', () => {
             console.log('q', el.def.reload)
             // el.def.reload.focus();
             this.run(o);
