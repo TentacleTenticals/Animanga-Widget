a531f11056c989b761c2d13f5a0f6855765d43f6
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2025-06-12 17:58:44 +0300

    Update m.js

--- api/ani/m.js
+++ api/ani/m.js
@@ -246,7 +246,7 @@ export class AniApi{
         `;
         const variables = {
           type: o.type.toUpperCase(),
-          search: o.title.slice(0, 64),
+          search: o.title,
           perPage: o.limit
         };
         o.data = {
